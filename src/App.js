2c1abb690c7934f688a1a7a85195a72a2c6190d4
dougb99 <douglaswburkinshaw@hotmail.co.uk>
2020-03-12 13:05:35 +0000

    w

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
-//jest-environment-jsdom-global
-//       "^.+\\.(js|jsx|mjs)$": "<rootDir>/node_modules/fetchai-ledger-api",
-
-class App extends Component {
-
-  render () {
-
-    window.hello = 9
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save!!! to reload.
-        </p>
-      </div>
-    )
-  }
-}
-
-export default App
+// import React, { Component } from 'react'
+// import logo from './logo.svg'
+// import './App.css'
+//
+// class App extends Component {
+//
+//   render () {
+//     window.hello = 9
+//
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo"/>
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save!!! to reload.
+//         </p>
+//       </div>
+//     )
+//   }
+// }
+//
+// export default App
